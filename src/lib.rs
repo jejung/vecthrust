a287e84e3c05b86ec36ad3a3d8ba378870371bfb
Jean Jung <jean.jung@rocketmail.com>
2017-04-26 02:46:08 -0300

    initial Vector method implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,77 @@
 //! The vecthrust crate includes functionality for working with linear algebra.
 
-pub struct Vector<'a> {
-    coordinates: &'a Vec<f64>,
-    dimension: usize,
+#[derive(Debug)]
+pub struct Vector {
+    pub dimension: usize,
+    pub coordinates: Vec<f64>,
 }
 
-impl<'a> Vector<'a> {
-    fn new(coordinates: &'a Vec<f64>) -> Vector {
+impl Vector {
+
+    /// Create a new Vector from a builtin Vec type array.
+    /// A Vector is a representation of geometric vectors as we know,
+    /// they are composed of coordinates and a dimension representing
+    /// how many axis are involved.
+    ///
+    /// # Examples;
+    ///
+    /// ```
+    /// use vecthrust::Vector;
+    /// let vector = Vector::new(vec![1.0, 1.0]);
+    /// assert_eq!(2, vector.dimension);
+    /// ```
+    pub fn new(coordinates: Vec<f64>) -> Vector {
+        if coordinates.len() == 0 {
+            panic!("Empty vectors are not allowed");
+        }
         Vector{
+            dimension: coordinates.len(),
             coordinates: coordinates,
-            dimension: coordinates.len()
         }
     } 
+
+    /// Format this vector as a string
+    /// # Examples:
+    ///
+    /// ```
+    /// use vecthrust::Vector;
+    ///
+    /// let vector = Vector::new(vec![1.0, 2.5099]);
+    /// assert_eq!("Vector: [1, 2.5099]", vector.to_string());
+    /// ```
+    pub fn to_string(&self) -> String {
+        return format!("Vector: {:?}", self.coordinates);
+    }
 }
+
+impl PartialEq for Vector {
+
+    /// Compares two vectors in order that:
+    ///
+    /// ```
+    /// use vecthrust::Vector;
+    ///
+    /// let vector1 = Vector::new(vec![1.0, 2.5099]);
+    /// let vector2 = Vector::new(vec![1.0, 2.5099]);
+    /// let vector3 = Vector::new(vec![2.0, 2.5099]);
+    /// assert!(vector1 == vector2);
+    /// assert!(!(vector1 == vector3));
+    /// ```
+    fn eq(&self, other: &Vector) -> bool {
+        return self.coordinates == other.coordinates;
+    }
+}
+
+impl Eq for Vector {}
 
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
-    fn test_dimension_syncronism() {
-         let values = vec![1.0, 1.0]; 
-         let vector = super::Vector::new(&values);
-         assert_eq!(2, vector.dimension);
+    #[should_panic(expected= "Empty vectors are not allowed")]
+    fn test_create_with_empty_coordinates() {
+        let vector = Vector::new(vec![]);
     }
 }
